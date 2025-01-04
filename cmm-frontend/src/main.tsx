d3de4e0f6d0e536ab2725436a2b60fd5c85c7b55
Kalyanmoy Borah <kalyanmoy@techvariable.com>
2025-01-04 22:12:32 +0530

    pages

--- cmm-frontend/src/main.tsx
+++ cmm-frontend/src/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { muiTheme } from "./themes/material.theme.ts";
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={muiTheme}>
+      <CssBaseline enableColorScheme />
       <App />
     </ThemeProvider>
   </StrictMode>
