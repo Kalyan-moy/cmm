b22a62adc4f8ef0f0fe3663b2e9ba80cbb2b2298
Kalyanmoy Borah <kalyanmoy@techvariable.com>
2025-01-05 12:43:22 +0530

    docker setup

--- cmm-backend/src/routes/api.ts
+++ cmm-backend/src/routes/api.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send("API is working!");
+  res.send("API is working fine!");
 });
 
 export default router;
