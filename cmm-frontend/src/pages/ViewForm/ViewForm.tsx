ebb21f7630822b770ee71e5826cb72989ec3deab
Kalyanmoy Borah <kalyanmoy@techvariable.com>
2025-01-05 21:03:27 +0530

    forms apis

--- cmm-frontend/src/pages/ViewForm/ViewForm.tsx
+++ cmm-frontend/src/pages/ViewForm/ViewForm.tsx
@@ -1,15 +1,21 @@
+import Navbar from "@/components/Navbar";
 import { useGetFormById } from "@/services/queries/forms.query";
+import { Box } from "@mui/material";
 import { useParams } from "react-router-dom";
 
 const ViewForm = () => {
   const params = useParams();
   const formId = params?.id ? +params.id : undefined;
 
-  const { data, isLoading } = useGetFormById(formId);
+  const { data } = useGetFormById(formId);
 
   console.log({ data });
 
-  return <div>ViewForm</div>;
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
 };
 
 export default ViewForm;
